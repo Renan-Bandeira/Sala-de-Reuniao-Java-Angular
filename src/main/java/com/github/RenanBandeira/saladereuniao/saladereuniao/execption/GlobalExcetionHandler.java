ed14ca53f55546ae89c33ca50e383117ddb9ce6e
renanbandeira94 <67717214+Renan-Bandeira@users.noreply.github.com>
2021-02-10 16:47:23 -0300

    Developed GlobalExcetionHandler

--- src/main/java/com/github/RenanBandeira/saladereuniao/saladereuniao/execption/GlobalExcetionHandler.java
+++ src/main/java/com/github/RenanBandeira/saladereuniao/saladereuniao/execption/GlobalExcetionHandler.java
@@ -1,4 +1,24 @@
 package com.github.RenanBandeira.saladereuniao.saladereuniao.execption;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
 public class GlobalExcetionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> ResourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+        ErrorDetails errorDetails=new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> GlobalExcetionHandler(Exception ex, WebRequest request){
+        ErrorDetails errorDetails=new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
